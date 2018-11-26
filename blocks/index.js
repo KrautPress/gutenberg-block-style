c54c411fef6109f96608bbb6113e87d34b8791a0
Florian Brinkmann <kontakt@florianbrinkmann.com>
2018-11-26 14:47:00 +0100

    Adding semicolon

--- blocks/index.js
+++ blocks/index.js
@@ -3,4 +3,4 @@ wp.domReady( () => {
 	const { registerBlockStyle } = wp.blocks;
 	registerBlockStyle( 'core/list', { name: 'default', label: 'Standard' } );
 	registerBlockStyle( 'core/list', { name: 'large', label: 'Groß' } );
-} )
+} );
